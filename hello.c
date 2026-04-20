ed2ba683b0dcbe4d24c2c65aa02935dcf8218872
zarisharagon-coder <zarisharagon@gmail.com>
2026-04-20 14:53:14 +0000

    recipe progress

--- hello.c
+++ hello.c
@@ -4,3 +4,4 @@ int main(void)
 {
     printf("Hello EnriqueZarishAndalonIbarra \n");
 }
+
---
+++ recipe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+printf("Coconut Cranberry Dessert\n");
+printf("1 1/2 cups graham cracker crumbs (about 24 full-sized graham crackers)\n
+
+1/2 cup butter or margarine, melted\n1 can (12 ounces/340 g) sweetened condensed milk\n1 cup flaked coconut\n
+1 cup dried cranberries\n2 cups vanilla chips\n1 cup sliced almonds or pecans");
+}
+
+
