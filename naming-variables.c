481f36964aac5a1a50a343403a2f06a602de73be
zarisharagon-coder <zarisharagon@gmail.com>
2026-04-21 14:50:03 +0000

    namingvariables

---
+++ naming-variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+int number;//i fixed the name of this variable ny deleting the exclamation sign//
+int test_value;//i fixed the code by adding an underscore
+int example; //i fixed the code by deleting the 1//
+float int_count ;
+printf("Variables created successfully.\n");
+}
