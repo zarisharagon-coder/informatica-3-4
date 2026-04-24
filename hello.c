fd7c770783f7d86e077f34cee63941abd08240bc
zarisharagon-coder <zarisharagon@gmail.com>
2026-04-24 16:35:34 +0000

    checkpint

--- hello.c
+++ hello.c
@@ -2,15 +2,16 @@
 
 int main(void)
 {
-   char name[50];
-    printf("Enter your name: ");
-    scanf("%s", &name, &);
-    printf("Hello %s!\n",name);
+char name[50];
+printf("Enter your name: ");
+//the scanf is for the whatever this is to ask you your name
+scanf("%s", &name);
+printf("Hello %s! \n",name);
 
-    char color[50];
-    printf("enter your favorite color: ");
-    scanf("%s", &color);
-    printf("%s sucks!")
+char color[50];
+printf("enter your favorite color: ");
+scanf("%s", &color);
+printf("%s is the worst color ive ever heard of! \n",color );
 }
 
 
---
+++ rectangle.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+float length, width, area;
+printf(20cm);
+scan("%f",&20cm)
+printf()
