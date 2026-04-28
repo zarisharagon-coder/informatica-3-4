729452b44b738c1d0b89d1740adf169b06c41fb5
zarisharagon-coder <zarisharagon@gmail.com>
2026-04-28 15:46:44 +0000

    minutes

---
+++ minutes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+    int minutes;
+    int start_time;
+    int movie_duration;
+    int end_time;
+    int minutes_left
+    printf("Enter the Lenght of the movie in minutes:");
+    scanf("%d", &minutes);
+    printf("Enter the time when you will start the movie");
+    scanf("%d",&start_time);
+    movie_duration= (minutes/60);
+    end_time= (start_time + movie_duration);
+    minutes_left
+    printf("movie duration: %d hours and %d minutes\n",movie_duration,minutes_left);
+    printf("The movie ends at : %d\n", end_time);
+    }
+
