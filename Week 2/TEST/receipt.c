34f06206ae6936d082eefd04f5302314a50c08e8
zarisharagon-coder <zarisharagon@gmail.com>
2026-04-29 14:54:50 +0000

    test

---
+++ Week 2/TEST/receipt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    int item;
+    int price;
+    int apple;
+    int result;
+
+    printf("Enter the desired item:\n");
+    scanf("%d",&item);
+    printf("Enter the price of the item\n");
+    scanf("%d",&price);
+    printf("Enter the quantity bought\n");
+    item= (apple);
+    apple =5;
+    result=(apple);
+
+    printf("Your total is: %d result" );
+}
+
+
